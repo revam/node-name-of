f3b716878fb2069f1f20c0685e9d754bad2f65b5
Mikal Stodal <mikalstordal@gmail.com>
2017-08-20 18:47:56 +0200

    Update index.js
    + Add lots of comments,
    * modify the regular expressions,
    + add semicolon at the end of each (non-bracket) statement,
    * always known type first conditional expersions,
    * throw an error and not a string,
    * and add brackets to if statements.

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
 /** Copyright (c) 2017 Mikal Stordal <mikalstordal@gmail.com> | MIT licensed */
-'use strict'
+'use strict';
 
 /*!
  * Constants
  */
-const classRegex = /class +([^ \n\r]+) *(?: [e]|\{)/
-const functionRegex = /function(?: *([^ \n\r]+) *\(| *(\())/
+// will work on __any__ valid class
+const classRegex = /^class[ \t]+([^ \t\n\r]+)[ \t]*(?:[ \t]e|\{)/
+// will work on __any__ valid function
+const functionRegex = /^function(?:[ \t]+([^ \t\n\r]+)[ \t]*\(|[ \t]*(\())/
 
 /**
  * Gets contructor name of `target`.
- * 
+ *
  * @param {Object|*} target Target object
  */
 function nameOf (target) {
-  let string
+  let string;
   if ('function' === typeof target) {
-    if (target.prototype !== Object.prototype
-    &&  target.prototype[Symbol.toStringTag] !== undefined)
-      return target.prototype[Symbol.toStringTag]
+    // If target.prototype is not the same as Object.prototype
+    if (Object.prototype !== target.prototype
+    // and it has a string tag
+    &&  undefined !== target.prototype[Symbol.toStringTag]) {
+      // return tag
+      return target.prototype[Symbol.toStringTag];
+    }
 
-    if (target === Object)
-      return 'Object'
+    // if target is Object,
+    if (Object === target) {
+      // return 'Object'
+      return 'Object';
+    }
 
-    string = target.toString()
+    // search constructor
+    string = target.toString();
   }
 
-  else if ('object' !== typeof target)
-    return undefined
+  else if ('object' !== typeof target) {
+    return;
+  }
 
   else {
-    if (target[Symbol.toStringTag] !== undefined)
-      return target[Symbol.toStringTag]
+    // if target has a string tag
+    if (undefined !== target[Symbol.toStringTag]) {
+      // return tag
+      return target[Symbol.toStringTag];
+    }
 
-    if (target.constructor === Object)
-      return 'Object'
+    // if target constructor is Object,
+    if (Object === target.constructor) {
+      // return 'Object'
+      return 'Object';
+    }
 
-    string = target.constructor.toString()
+    // for every other object, search constructor
+    string = target.constructor.toString();
   }
 
-  let results
+  let results;
   switch (string[0]) {
+    // get class name
     case 'c':
-      results = classRegex.exec(string)
-      return results? results[1] : ''
+      results = classRegex.exec(string);
+      return results? results[1] : '';
 
+    // get function name
     case 'f':
-      results = functionRegex.exec(string)
-      if (undefined !== results[2])
-        return '<anonymous constructor>'
-      return results? results[1] : ''
+      results = functionRegex.exec(string);
+      if (undefined !== results[2]) {
+        // return anonymous when no name is found
+        return '<anonymous constructor>';
+      }
+      return results? results[1] : '';
 
+    // since we came this far,
     default:
-      throw `unable to get name of target`
+      // it's not expected behavior to __not__ return.
+      throw new Error(`unable to get name of '${string}'`);
   }
 }
 

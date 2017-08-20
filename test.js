7b292d12e38882ef1c6c27c139213483711d929c
Mikal Stodal <mikalstordal@gmail.com>
2017-08-20 18:53:37 +0200

    Fix test.js
    + Add missing/incomplete tests.
    + Add some more conditions to some tests.
    * Fix direction of 'expected' and 'actual' in assert.*.
    + Add semicolon at the end of each (non-bracket) statement.

--- test.js
+++ test.js
@@ -1,66 +1,70 @@
-const assert = require('assert')
-const nameOf = require('.')
+const assert = require('assert');
+const nameOf = require('.');
 
-class TestClass1 {}
-
-function TestClass2 () {}
-
-class TestClass3 extends TestClass1 {}
-
-const TestClass4 = function () {}
-
-class TestClass5 { get [Symbol.toStringTag] () { return 'TestClassStringTag' } }
+// Test classes/functions
+class TestCase1 {}
+function TestCase2 () {}
+class TestCase3a extends TestCase1 {}
+class TestCase3b extends TestCase2 {}
+const TestCase4a = function () {};
+const TestCase4b = function(){};
+class TestCase5 { get [Symbol.toStringTag] () { return 'TestClassStringTag' } }
+const TestCase6 = { [Symbol.toStringTag]: 'TestCase6' };
 
 describe ('nameOf', () => {
-  it ('should return `undefined` when target is not an object', () => {
-    assert.equal (undefined, nameOf (''))
+  it ('should return `undefined` when target is not an object or function', () => {
+    assert.equal (nameOf (''), undefined);
+    assert.equal (nameOf (0), undefined);
+    assert.equal (nameOf (NaN), undefined);
+    assert.equal (nameOf (Infinity), undefined);
+    assert.equal (nameOf (false), undefined);
   })
 
   it ('should return class name for standard classes', () => {
-    let instance = new TestClass1
-    
+    assert.equal (nameOf (new TestCase1), 'TestCase1');
   })
 
   it ('should return function name for standard functions', () => {
-    let instance = new TestClass2
-
+    assert.equal (nameOf (new TestCase2), 'TestCase2');
   })
-  
-  it ('should be work on extended classes', () => {
-    let instance = new TestClass3
 
+  it ('should work on extended classes', () => {
+    assert.equal (nameOf (new TestCase3a), 'TestCase3a');
+    assert.equal (nameOf (new TestCase3b), 'TestCase3b');
   })
 
   it ('should return `<anonymous constructor>` for objects constructed from a constructor without a name', () => {
-    let instance = new TestClass4
-
-    assert.equal ('<anonymous constructor>', nameOf (instance))
+    assert.equal (nameOf (new TestCase4a), '<anonymous constructor>');
+    assert.equal (nameOf (new TestCase4b), '<anonymous constructor>');
   })
 
   it ('should respect `Symbol.toStringTag` above all, if present', () => {
-    let instance = new TestClass5
-    
-    assert.notEqual ('TestClass5', nameOf (instance))
-    assert.equal ('TestClassStringTag', nameOf (instance))
+    let instance = new TestCase5
+
+    assert.notEqual (nameOf (instance), 'TestCase5');
+    assert.equal (nameOf (instance), 'TestClassStringTag');
+    assert.equal (nameOf (TestCase6), 'TestCase6');
   })
 
   it ('should work directly on constructors', () => {
-    assert.equal ('TestClass1', nameOf (TestClass1))
-    assert.equal ('TestClass2', nameOf (TestClass2))
-    assert.equal ('TestClass3', nameOf (TestClass3))
-    assert.equal ('<anonymous constructor>', nameOf (TestClass4))
-    assert.equal ('TestClassStringTag', nameOf (TestClass5))
+    assert.equal (nameOf (TestCase1), 'TestCase1');
+    assert.equal (nameOf (TestCase2), 'TestCase2');
+    assert.equal (nameOf (TestCase3a), 'TestCase3a');
+    assert.equal (nameOf (TestCase3b), 'TestCase3b');
+    assert.equal (nameOf (TestCase4a), '<anonymous constructor>');
+    assert.equal (nameOf (TestCase4b), '<anonymous constructor>');
+    assert.equal (nameOf (TestCase5), 'TestClassStringTag');
   })
 
   it ('should work on any objects', () => {
-    assert.equal ('Array', nameOf ([]))
-    assert.equal ('RegExp', nameOf (/d/))
-    assert.equal ('Object', nameOf ({}))
-    assert.equal ('Date', nameOf (new Date))
+    assert.equal (nameOf ([]), 'Array')
+    assert.equal (nameOf (/d/), 'RegExp')
+    assert.equal (nameOf ({}), 'Object')
+    assert.equal (nameOf (new Date), 'Date')
 
-    assert.equal ('Array', nameOf (Array))
-    assert.equal ('RegExp', nameOf (RegExp))
-    assert.equal ('Object', nameOf (Object))
-    assert.equal ('Date', nameOf (Date))
+    assert.equal (nameOf (Array), 'Array')
+    assert.equal (nameOf (RegExp), 'RegExp')
+    assert.equal (nameOf (Object), 'Object')
+    assert.equal (nameOf (Date), 'Date')
   })
 })
